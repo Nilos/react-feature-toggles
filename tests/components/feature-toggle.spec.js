582b960bbbec6c54dd43b6fe2523bc42b19c3f4d
Nils Kenneweg <nk@ovt.me>
2017-02-15 14:54:23 +0100

    tests for basic feature toggle

--- tests/components/feature-toggle.spec.js
+++ tests/components/feature-toggle.spec.js
@@ -1,14 +1,35 @@
+import React from 'react'
 import chai, { expect } from 'chai'
 import dirtyChai from 'dirty-chai'
-import { FeatureToggle } from '../../src/index'
+import { shallow } from 'enzyme'
+import { FeatureToggle, setFeatureConfig } from '../../src/index'
 
 chai.use(dirtyChai)
 
 const { describe, it } = global
 
-describe('feature toggle component', () => {
-  it('renders children if toggle is enabled')
-  it('ignores children if toggle is disabled')
+describe('<FeatureToggle/>', () => {
+  it('renders children if toggle is enabled', () => {
+    setFeatureConfig({ testFeature: true })
+
+    const wrapper = shallow(
+      <FeatureToggle name="testFeature">
+        <div>Test</div>
+      </FeatureToggle>)
+
+    expect(wrapper.find('div')).to.have.length(1)
+  })
+
+  it('ignores children if toggle is disabled', () => {
+    setFeatureConfig({ testFeature: false })
+
+    const wrapper = shallow(
+      <FeatureToggle name="testFeature">
+        <div>Test</div>
+      </FeatureToggle>)
+
+    expect(wrapper.find('div')).to.have.length(0)
+  })
 
   it('adds routes if toggle is enabled')
   it('ignores routes if toggle is disabled')
