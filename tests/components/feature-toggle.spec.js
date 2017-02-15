9daf915306ddac1e39748d0e89c720af766b3890
Nils Kenneweg <nk@ovt.me>
2017-02-15 14:59:57 +0100

    add specs for feature toggle

--- tests/components/feature-toggle.spec.js
+++ tests/components/feature-toggle.spec.js
@@ -2,6 +2,8 @@ import React from 'react'
 import chai, { expect } from 'chai'
 import dirtyChai from 'dirty-chai'
 import { shallow } from 'enzyme'
+import { Route, IndexRoute, match } from 'react-router'
+
 import { FeatureToggle, setFeatureConfig } from '../../src/index'
 
 chai.use(dirtyChai)
@@ -31,6 +33,49 @@ describe('<FeatureToggle/>', () => {
     expect(wrapper.find('div')).to.have.length(0)
   })
 
-  it('adds routes if toggle is enabled')
-  it('ignores routes if toggle is disabled')
+  it('adds routes if toggle is enabled', (done) => {
+    setFeatureConfig({ testFeature: true })
+
+    const routes = (
+      <Route name="home" path="/">
+        <IndexRoute />
+        <FeatureToggle name="testFeature">
+          <Route name="route1" path="route1">
+            <IndexRoute />
+            <Route name="route1.sub" path=":routeId">
+              <IndexRoute />
+            </Route>
+          </Route>
+        </FeatureToggle>
+        <Route name="route2" path="" />
+      </Route>)
+
+    match({ routes, location: '/route1/5' }, (err, redirect, props) => {
+      expect(props).to.not.be.undefined()
+      done()
+    })
+  })
+
+  it('ignores routes if toggle is disabled', (done) => {
+    setFeatureConfig({ testFeature: false })
+
+    const routes = (
+      <Route name="home" path="/">
+        <IndexRoute />
+        <FeatureToggle name="testFeature">
+          <Route name="route1" path="route1">
+            <IndexRoute />
+            <Route name="route1.sub" path=":routeId">
+              <IndexRoute />
+            </Route>
+          </Route>
+        </FeatureToggle>
+        <Route name="route2" path="" />
+      </Route>)
+
+    match({ routes, location: '/route1/5' }, (err, redirect, props) => {
+      expect(props).to.be.undefined()
+      done()
+    })
+  })
 })
